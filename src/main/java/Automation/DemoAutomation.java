5db2eb88adefadbe07f6f6af14fb587744b02f8b
Shahrukh <themrsrk@gmail.com>
2021-12-29 18:16:44 +0500

    Lectures include 45 to 54

--- src/main/java/Automation/DemoAutomation.java
+++ src/main/java/Automation/DemoAutomation.java
@@ -1,16 +1,23 @@
 package Automation;
 
+import java.time.Duration;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -22,6 +29,7 @@ public class DemoAutomation {
 	public static String browser = "chrome";
 	
 	
+	@SuppressWarnings("deprecation")
 	public static void main (String args[]) throws InterruptedException {
 	
 	if ( browser.equals("chrome"))  {
@@ -131,15 +139,163 @@ public class DemoAutomation {
 //	
 //	driver.findElement(By.name("UserFirstName")).sendKeys("Shahrukh");
 //	driver.findElement(By.name("UserLastName")).sendKeys("Siddiqui");
+//	
 	
-	driver.get("https://www.salesforce.com/in/?ir=1");
-	driver.manage().window().maximize();
+	//How to handle Mouse hover
+//	driver.get("https://www.salesforce.com/in/?ir=1");
+//	driver.manage().window().maximize();
+//	
+//	WebElement element = driver.findElement(By.xpath("//*[@id=\"support-service_menu_item\"]/button/span[1]"));
+//	
+//	Actions action = new Actions(driver); //this is the action class which is used to perform mouse hover functionalities
+//	
+//	action.moveToElement(element).perform();
 	
-	WebElement element = driver.findElement(By.xpath("//*[@id=\"support-service_menu_item\"]/button/span[1]"));
 	
-	Actions action = new Actions(driver); //this is the action class which is used to mouse hover functionalities
 	
-	action.moveToElement(element).perform();
+//	//How to handle drag and drop actions
+//		driver.get("https://jqueryui.com/droppable/");
+//		driver.manage().window().maximize();
+//		
+//		WebElement frame = driver.findElement(By.xpath("//*[@id=\"content\"]/iframe"));
+//		driver.switchTo().frame(frame);
+//		
+//		
+//		WebElement drag = driver.findElement(By.id("draggable"));
+//		WebElement drop = driver.findElement(By.id("droppable"));
+//		
+//		Actions action = new Actions(driver);
+//		action.dragAndDrop(drag, drop).perform(); //this is drag and drop method with 2 parameters source which is dragable element and destination which is droppable element
+	
+	
+	
+	
+//	//How to handle resizeable elements
+//		driver.get("https://jqueryui.com/resizable/");
+//		driver.manage().window().maximize();
+//		
+//		WebElement frame = driver.findElement(By.xpath("//*[@id=\"content\"]/iframe"));
+//		driver.switchTo().frame(frame);
+//		
+//		
+//		WebElement drag = driver.findElement(By.xpath("//*[@id=\"resizable\"]/div[3]"));
+//		//WebElement drop = driver.findElement(By.id("droppable"));
+//		
+//		Actions action = new Actions(driver);
+//		action.dragAndDropBy(drag, 100, 150).perform(); //this is drag and drop by offset method with 2 parameters source which is dragable element and offsets which are axis in X and Y
+		
+	
+	
+	
+////	//How to handle Slider 
+//	driver.get("https://jqueryui.com/slider/");
+//	driver.manage().window().maximize();
+//	
+//	WebElement frame = driver.findElement(By.xpath("//*[@id=\"content\"]/iframe"));
+//	driver.switchTo().frame(frame);
+//	
+//	
+//	WebElement slider = driver.findElement(By.xpath("//*[@id=\"slider\"]/span"));
+//	//WebElement drop = driver.findElement(By.id("droppable"));
+//	
+//	Actions action = new Actions(driver);
+//	action.dragAndDropBy(slider, 100, 150).perform();
+//	Thread.sleep(2000);
+//	action.dragAndDropBy(slider, -100, 150).perform();	//y value is not changing thats why we have done change in X cordinate
+		
+		
+	
+//////How to handle Right click 
+//driver.get("https://jqueryui.com/slider/#colorpicker");
+//driver.manage().window().maximize();
+//
+//WebElement frame = driver.findElement(By.xpath("//*[@id=\"content\"]/iframe"));
+//driver.switchTo().frame(frame);
+//
+//
+////WebElement slider = driver.findElement(By.xpath("//*[@id=\"slider\"]/span"));
+////WebElement drop = driver.findElement(By.id("droppable"));
+//
+//WebElement swatch = driver.findElement(By.xpath("//*[@id=\"swatch\"]"));
+//
+//Actions action = new Actions(driver);
+//action.contextClick(swatch).perform();	// contextclick is the right click method
+	
+	
+	
+//	//How to handle Keyboard Events
+//	driver.get("https://extendsclass.com/text-compare.html");
+//	driver.manage().window().maximize();
+//
+//	WebElement source = driver.findElement(By.xpath("//*[@id=\"dropZone\"]/div[2]/div/div[6]/div[1]/div/div/div/div[5]/div[7]/pre"));
+//	WebElement destination = driver.findElement(By.xpath("//*[@id=\"dropZone2\"]/div[2]/div/div[6]/div[1]/div/div/div/div[5]/div[7]/pre"));
+////	driver.switchTo().frame(frame);
+//
+//	
+//	//WebElement slider = driver.findElement(By.xpath("//*[@id=\"slider\"]/span"));
+//	//WebElement drop = driver.findElement(By.id("droppable"));
+//
+////	WebElement swatch = driver.findElement(By.xpath("//*[@id=\"swatch\"]"));
+//
+//	Actions action = new Actions(driver);
+//	action.keyDown(source, Keys.COMMAND).sendKeys("a").sendKeys("c").build().perform(); //here we've used build method because here are multiple actions so when we have multiple actions we used build for sequence
+//	action.keyDown(destination, Keys.COMMAND).sendKeys("a").sendKeys("v").build().perform();
+	
+	
+	
+	
+//How to perform Implicit waits
+//	driver.get("https://www.ebay.com/");
+//	driver.manage().window().maximize();
+//	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //this is implicit wait we use this wait so as soon as webdriver found an element it performs actions and it won't wait for given time, the maximum, this is the max wait in parameters
+//	//and it is applicable for all the webelements on the webpage
+//	
+//
+//	
+//	WebElement hover_element = driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[1]/ul/li[3]/a"));
+//
+//	Actions action = new Actions(driver); //this is the action class which is used to perform mouse and keyboard functions
+//	
+//	action.moveToElement(hover_element).perform();
+//	driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[1]/ul/li[3]/div[2]/div[1]/nav[2]/ul/li[1]/a")).click();
+
+	
+	
+	//How to handleExplicit waits.
+//	driver.get("https://www.ebay.com/");
+//	driver.manage().window().maximize();
+//
+//	
+//	WebElement hover_element = driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[1]/ul/li[3]/a"));
+//
+//	Actions action = new Actions(driver); //this is the action class which is used to perform mouse and keyboard functions
+//	
+//	action.moveToElement(hover_element).perform();
+//	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));     //this is explicit wait we use this wait when webelemet takes more time than the given time, e:g here in this example if the web element takes more than 10 seconds, it will handle
+//
+//	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"mainContent\"]/div[1]/ul/li[3]/div[2]/div[1]/nav[2]/ul/li[1]/a"))).click();
+	
+	
+	
+
+//	//How to perform Fluent wait
+//	driver.get("https://www.ebay.com/");
+//	driver.manage().window().maximize();
+//
+//	
+//	WebElement hover_element = driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[1]/ul/li[3]/a"));
+//
+//	Actions action = new Actions(driver); //this is the action class which is used to perform mouse and keyboard functions
+//	
+//	action.moveToElement(hover_element).perform();
+//	Wait<WebDriver> wait = new FluentWait<WebDriver>(driver) //this is fluent wait we use this wait when we want to create our custom error message or something else
+//		       .withTimeout(Duration.ofSeconds(10))
+//		       .pollingEvery(Duration.ofSeconds(2))
+//		       .withMessage("This is Custom Error Message")
+//		       .ignoring(NoSuchElementException.class);
+//	
+//	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"mainContent\"]/div[1]/ul/li[3]/div[2]/div[1]/nav[2]/ul/li[1]/a"))).click();
+//	
 	
 }
 }
